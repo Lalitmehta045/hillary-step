f72c665e3d133d9d375a6034f6f229f1ef39d547
RohitS19K <162042107+RohitS19K@users.noreply.github.com>
2026-09-08 17:03:55 +0530

    Update Footer.tsx

--- frontend/components/site/Footer.tsx
+++ frontend/components/site/Footer.tsx
@@ -168,8 +168,8 @@ export function Footer() {
               <Image
                 src="/assets/HillaryStepSolutionLogo.png"
                 alt="Hillary Step Solutions Logo"
-                width={120}
-                height={43}
+                width={180}
+                height={90}
                 className="object-contain"
               />
               <p className="font-display text-[20px] font-[400] leading-[28px] tracking-[-0.5px] text-white">
